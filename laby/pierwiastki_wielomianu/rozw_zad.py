128cce07dee6eb5ed657c8251809636e933a9242
TheNuRii <maciekglow0@gmail.com>
2023-12-09 17:52:51 +0100

    Dzialajace rozwazanie - pierwiastki_wielominu

--- laby/pierwiastki_wielomianu/rozw_zad.py
+++ laby/pierwiastki_wielomianu/rozw_zad.py
@@ -1,29 +1,29 @@
 import numpy as np
 import json
 
-def wylicz_pierwistek(wyraz_wolny, wielomian):
-    for i in range(1, wielomian.shape[0]):
-        print(i)
+def  wylicz_pierwistek(wyraz_wolny, wielomian):
+    print(f"pierwiastki podanego wielomianu wynosza: ")
+    czy_nie_posiada_pierwiastkow = True
+    for i in range(1, abs(wyraz_wolny) + 1):
         if wyraz_wolny % i == 0:
             czy_pierwiastek = 0
             for j in range(1, wielomian.shape[0]):
                 czy_pierwiastek += i ** j * wielomian[j]
             if (czy_pierwiastek + wyraz_wolny) == 0:
-                return i
+                print(f"{i}, ")
+                czy_nie_posiada_pierwiastkow = False
             czy_pierwiastek = 0
             for j in range(1, wielomian.shape[0]):
                 czy_pierwiastek += (-i)**j * wielomian[j]
-                print(czy_pierwiastek)
+            
             if (czy_pierwiastek + wyraz_wolny) == 0:
-                return -i
-
-    return "Wielomian nie ma rozwiazan"
+                print(f"{-i}, ")
+                czy_nie_posiada_pierwiastkow = False
+    if czy_nie_posiada_pierwiastkow:
+        print( "Wielomian nie ma rozwiazan")
 
 wielomian = np.array(json.loads(input("Podaj wspolczynki wielomianu w postaci wektora (index == stopien potegi przy x): ")))
 ilosc_wspolczynkiow = wielomian.shape[0]
 
 wyraz_wolny = wielomian[0]
-pierwiastki = np.zeros(ilosc_wspolczynkiow)
-pierwiastki[0] = wylicz_pierwistek(wyraz_wolny, wielomian)
-
-print(pierwiastki)
+wylicz_pierwistek(wyraz_wolny, wielomian)
