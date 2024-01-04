e89b9f752d6052c5a861c3e6d00b9d2a1f3207e6
TheNuRii <maciekglow0@gmail.com>
2024-01-04 17:55:28 +0100

    Rekurencyjnie Taylor

--- laby/rekurencyjne_cos(x)/rek_cos.py
+++ laby/rekurencyjne_cos(x)/rek_cos.py
@@ -1,42 +1,46 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 #obliczyc na kartce an a bardziej roznice k
 def cos_rek(x, N, err):
-    an = 0
-    y=x
+    an = 1
+    y=1
     for i in range(1, N):
-        an += 1
-
+        an = an*(-x*x/(2*i*(2*i)))
+        #(-x * x/((2*i+ 2)*(2*i + 1)))
+        if abs(an) < err:
+            break
         y += an
-    
-    return y
+    return y, i
     
 
 x = np.linspace(-10, 10, 300)
 
 y_rek = np.zeros(len(x))
 y_wbudowane = np.cos(x)
+liczba_iteracji = np.zeros(len(x))
 
 N = 8
 err = 0.1
+
 for i in range(len(x)):
-    y_rek[i] = cos_rek(x[i], N, err)
+    y_rek[i], liczba_iteracji[i] = cos_rek(x[i], N, err)
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
 plt.suptitle("Porównanie wykresów cos(x)")
 
-#ax1.ylabel("f(x) = cos(x)")
-#ax1.xlabel("x")
+ax1.set_ylabel("f(x) = cos(x)")
+ax1.set_xlabel("x")
 ax1.plot(x, y_wbudowane, label="Wykres wbudowanego cos(x)")
+ax1.plot(x, y_rek, 'r',label="Wykres cos(x) - rekurencja Tay")
 ax1.set_ylim([-3, 3])
 ax1.legend()
 ax1.grid()
 
-#ax2.ylabel("f(x) = cos(x)")
-#ax2.xlabel("x")
-ax2.plot(x, y_rek, label="Wykres cos(x) - rekurencja Tay")
-ax2.set_ylim([-3, 3])
+ax2.plot(x, liczba_iteracji, label="wykres liczby wymaganych iteracji Taylora")
+ax2.set_ylabel("liczba iteracji")
+ax2.set_xlabel("x")
 ax2.legend()
 ax2.grid()
 
--- laby/rekurencyjnie_exp(x)/rek_exp.py
+++ laby/rekurencyjnie_exp(x)/rek_exp.py
@@ -2,33 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def exp_rek(x, N, err):
-    an = 0
+    an = x
     y = x
     for i in range(1,N):
-        an += 1
-        an = y
-    
-    return y
+        an *= x / (i + 1)
+        if abs(an) < err:
+            break
+        y += an
+    return y, i
 
 x = np.linspace(-10, 10, 300)
-
-y_rek = np.zeros(len(x))
-y_wbudowane = np.exp(x)
-
 N =10
 err = 0.01
 
+y_rek = np.zeros(len(x))
+y_wbudowane = np.exp(x)
+liczba_iteracji = np.zeros(len(x))
 for i in range(len(x)):
-    y_rek[i] = exp_rek(x[i], N, err)
+    y_rek[i], liczba_iteracji[i] = exp_rek(x[i], N, err)
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
 plt.suptitle("Wyresy funkcji exponecjalnych")
 
 ax1.plot(x, y_wbudowane, label="Wykres wbudowanej funkcji exp(x)")
+ax1.plot(x, y_rek, label="Wykres exp(x) - rekurencyjnie Tay", color='r')
+ax1.set_xlabel("x")
+ax1.set_ylabel("f(x) = exp(x)")
 ax1.legend()
 ax1.grid()
 
-ax2.plot(x, y_rek, label="Wykres exp(x) - rekurencyjnie Tay")
+ax2.plot(x, liczba_iteracji, label="Wykres liczby wymaganych iteracji Taylora")
+ax2.set_xlabel('x')
+ax2.set_ylabel("liczba iteracji taylora")
 ax2.legend()
 ax2.grid()
 
