5f7f3e55d9682d492635c1ebb6c1b4c4f253ed4d
TheNuRii <maciekglow0@gmail.com>
2024-01-18 23:39:41 +0100

    Wylicznie pola po krzywa wielomianu za pomoca calki - metoda prostokata

--- laby/laby_2/calka_metoda_prostokata/calka_prostokat.py
+++ laby/laby_2/calka_metoda_prostokata/calka_prostokat.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 import json
 
 def calka_wielomian(wektor_wspolczynkiow, L_prostokatow, dolna_granica, gorna_granica):
-    pass
+    pole = 0
+    delta_x = (gorna_granica - dolna_granica)/L_prostokatow
+    srodki_przedziałów = np.linspace(dolna_granica + delta_x/2, gorna_granica - delta_x/2, L_prostokatow)
+    wysokosci_prostokatow = np.zeros(len(srodki_przedziałów))
+    for i in range(len(srodki_przedziałów)):
+        wysokosci_prostokatow[i] = roz_wielomianu(wektor_wspolczynkiow, srodki_przedziałów[i])
+        if wysokosci_prostokatow[i] < 0:
+            wysokosci_prostokatow[i] = 0
+
+        pole += delta_x * wysokosci_prostokatow[i]
+
+    return pole, wysokosci_prostokatow, srodki_przedziałów
 
 def roz_wielomianu(wielomian, x):
     suma = 0
@@ -12,21 +23,26 @@ def roz_wielomianu(wielomian, x):
     
     return suma
 
+
 wielomian =  np.array(json.loads(input("Podaj wielomian indexuj od wyrazu wolnego: ")))
 a_przedzial = float(json.loads(input("Podaj przedzial calkowania od a: ")))
 b_przedzial = float(json.loads(input("Podaj przedzial calkowania do b: ")))
 x_wiel = np.linspace(-10, 10, 1000)
 y_wiel = np.zeros(len(x_wiel))
+L_prostokatow = 20
 
 for i in range(len(x_wiel)):
     y_wiel[i] = roz_wielomianu(wielomian, x_wiel[i])
 
+pole, wysokosci_prostokatow, srodki_przedziałów = calka_wielomian(wielomian, L_prostokatow, a_przedzial, b_przedzial)
+
 plt.figure()
-plt.ylim(0, 1000)
 plt.ylabel("f(x)")
 plt.xlabel("x")
+plt.ylim(-1, 20)
+plt.bar(srodki_przedziałów, wysokosci_prostokatow)
 plt.plot(x_wiel, y_wiel, 'r')
 plt.grid()
 plt.show()
 
-
+print(f"Pole pod krzywą wielomianu wynosi: {pole}")
